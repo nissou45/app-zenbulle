79cf01f933cf319d7ea2eae65e0ad10ad8d74903
nissou45 <niss91@icloud.com>
2026-06-04 15:49:00 +0200

    fix: remove constants/roles.js dependency from isAdmin

--- backend/middlewares/isAdmin.js
+++ backend/middlewares/isAdmin.js
@@ -1,9 +1,7 @@
-const ROLES = require("../constants/roles");
-
 module.exports = (req, res, next) => {
   if (!req.session.user)
     return res.status(401).json({ message: "Non connecté" });
-  if (req.session.user.role !== ROLES.ADMIN)
+  if (req.session.user.role !== "admin")
     return res.status(403).json({ message: "Accès refusé" });
   next();
 };
